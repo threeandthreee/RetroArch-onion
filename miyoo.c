5e6171dbe8a4214fb1adccd9dfad4ec3d154df11
Aidan Cyr <aidan@aidancyr.dev>
2023-06-07 21:36:10 +1000

    Save core override file with new aspect / scaling values

--- miyoo.c
+++ miyoo.c
@@ -27,6 +27,42 @@ static void show_miyoo_fullscreen_notification(settings_t* settings){
    runloop_environment_cb(RETRO_ENVIRONMENT_SET_MESSAGE_EXT, &msg_obj);
 }
 
+static void write_core_override_aspect_scale(settings_t* settings){
+   char override_path[PATH_MAX_LENGTH] = {0};
+   char config_directory[PATH_MAX_LENGTH] = {0};
+   config_file_t* conf = NULL;
+
+   /* Used to get running core */
+   rarch_system_info_t* system = &runloop_state_get_ptr()->system;
+
+   /* Get base config directory */
+   fill_pathname_application_special(config_directory,
+         sizeof(config_directory),
+         APPLICATION_SPECIAL_DIRECTORY_CONFIG);
+
+   /* Get override file path - Core Override */
+   fill_pathname_join_special_ext(override_path,
+                  config_directory, system->info.library_name,
+                  system->info.library_name,
+                  FILE_PATH_CONFIG_EXTENSION,
+                  sizeof(override_path));
+
+   // Use existing override file, if exists, or create new config file
+   if (!(conf = config_file_new_from_path_to_string(override_path)))
+         conf = config_file_new_alloc();
+
+   // Set the two overrides - leave everything else as-is
+   config_set_string(conf, "video_dingux_ipu_keep_aspect",
+      (settings->bools.video_dingux_ipu_keep_aspect) ? "true" : "false");
+
+   config_set_string(conf, "video_scale_integer",
+      (settings->bools.video_scale_integer) ? "true" : "false");
+
+   // Write override file
+   bool ret = config_file_write(conf, override_path, false);
+   config_file_free(conf);
+}
+
 void miyoo_event_fullscreen_impl(settings_t* settings){
    /* Toggle Aspect & Scaling */
    settings->bools.video_dingux_ipu_keep_aspect = !settings->bools.video_dingux_ipu_keep_aspect;
@@ -37,5 +73,6 @@ void miyoo_event_fullscreen_impl(settings_t* settings){
    video_driver_apply_state_changes();
 
    show_miyoo_fullscreen_notification(settings);
+   write_core_override_aspect_scale(settings);
 }
 #endif
